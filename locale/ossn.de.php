ec17cfaefca4d73853a144e33ac3faeaedd4a2c8
Arsalan Shah <manactric@gmail.com>
2026-02-10 14:32:51 +0500

    Create ossn.de.php

---
+++ locale/ossn.de.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) Engr. Arsalan Shah
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+ossn_register_languages('de', array(
+			'favicons:generator:select' => 'Auswählen',
+			'favicons:generator:enabled' => 'Aktiviert',
+			'favicons:generator:disabled' => 'Deaktiviert',
+			'favicon:generator:sw:https:error' => 'Aktivierung von PWA / Service Worker nicht möglich – Ihre Seite muss HTTPS unterstützen.',
+			'favicon:generator:logo:dim:error' => 'Das Logo muss quadratisch sein und mindestens 512px * 512px groß sein',
+			'favicon:generator:logo:info' => 'Logo-Datei (mindestens 512px * 512px und quadratisch)',
+			'favicon:generator:logo:info:2' => 'Stellen Sie sicher, dass die Logo-Datei nicht mehr als 1 MB groß ist',
+			'favicon:generator:sw' => 'Service Worker aktivieren',
+			'favicon:generator:sw:info' => 'Wenn aktiviert, wird die Progressive Web App (PWA) aktiviert. Benutzer können die PWA auf unterstützten Geräten installieren. Stellen Sie sicher, dass Sie das SSL-Protokoll (https) verwenden.',
+			'favicon:generator:logo:error' => 'Ungültige Logo-Datei',
+			'favicon:generator:logo:done' => 'Das Logo wurde erstellt',
+			'favicon:generator:reset' => 'Jetzt zurücksetzen',
+			'favicon:generator:reset:info' => 'Die Symbole wurden bereits erstellt. Sie können zurücksetzen, um das Favicon Ihrer Website zu ändern.',
+));
